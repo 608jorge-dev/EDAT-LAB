d9f1cf1ed638ae8fb97dacb18cf8de2365215d22
ireyero07 <ireyero07.gmail.com>
2026-02-26 13:00:41 +0100

    Ejercicio 1 todo ya hecho en clase

--- material_P2/p2_e1.c
+++ material_P2/p2_e1.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "stack.h"
+#include "music.h"
+#include "radio.h"
 
 int show_player_menu(Stack *history);
 int show_player_menu(Stack *history) {
@@ -31,7 +34,70 @@ int show_player_menu(Stack *history) {
 
 /* TODO MAIN FUNCTION */
 int main (int argc, char **argv) {
+    Radio *r1=NULL;
+    FILE *fIn=fopen(argv[1], "r");
+    Music *m=NULL;
+    Stack *sl=NULL;
+    int i, option;
+    
+    if (argc<2) {
+        fprintf (stderr, "Error while reading %s file.\n", argv[0]);
+        return -1;
+    }
+    
+    if ((fIn == NULL)){
+        fprintf (stderr, "Error opening file.\n");
+        fclose(fIn);
+        return -1;
+    }
+    r1=radio_init();
+    if (r1==NULL)    {
+        fprintf(stderr, "Error while initializing the radio.\n");
+        fclose(fIn);
+        return -1;
+    }
+    if (radio_readFromFile(fIn,r1)==ERROR)  {
+        fprintf (stderr, "Error while reading the file.\n");
+        radio_free(r1);
+        fclose (fIn);
+        return -1;
+    }
+    
+    //Adding radio music to stack
+    sl=stack_init();
+    if (sl==NULL)    {
+        fprintf (stderr, "Error while initializing the stack.\n");
+        radio_free(r1);
+        fclose (fIn);
+        return -1;
+    }
+    for (i=0; i<radio_getNumberOfMusic(r1); i++)    {
+        m=radio_getMusic(r1,i);
+        if (m==NULL) {
+            fprintf (stderr, "Error while getting the music strutcure.\n");
+            radio_free(r1);
+            fclose (fIn);
+            stack_free(sl);
+            return -1;
+        }
+        stack_push(sl, m);
+    }
 
+    do {
+        option = show_player_menu(sl);
+
+        if (option == 1) {
+            stack_pop(sl);
+        }
+        
+    } while (option != 2);
+
+
+
+    //free r1, file and stack
+    radio_free(r1);
+    fclose (fIn);
+    stack_free(sl);
 }
 
 
