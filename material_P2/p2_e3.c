b9e4ae591255b381336bfad38325dc77fcbf3d32
ireyero07 <ireyero07@gmail.com>
2026-03-15 23:18:02 +0100

    Ahora funciona, pero no uso el get/set index en ningun lado, y no se si eso estara mal

--- material_P2/p2_e3.c
+++ material_P2/p2_e3.c
@@ -21,8 +21,7 @@ Status radio_depthSearch (Radio *r, long from_id, long to_id);
 int main(int argc, char **argv)
 {
     Radio *r1 = NULL;
-    FILE *fIn = fopen(argv[1], "r");
-    Stack *sl = NULL;
+    FILE *fIn;
     long from_id, to_id;
 
     if (argc < 4)
@@ -32,7 +31,7 @@ int main(int argc, char **argv)
     }
 
     from_id = strtol(argv[2], NULL, 10);
-    to_id   = strtol(argv[3], NULL, 10);
+    to_id = strtol(argv[3], NULL, 10);
 
     if (from_id < 0 || to_id < 0)
     {
@@ -40,12 +39,14 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    if ((fIn == NULL))
+    fIn = fopen(argv[1], "r");
+
+    if (fIn == NULL)
     {
         fprintf(stderr, "Error opening file.\n");
-        fclose(fIn);
         return -1;
     }
+
     r1 = radio_init();
     if (r1 == NULL)
     {
@@ -53,6 +54,7 @@ int main(int argc, char **argv)
         fclose(fIn);
         return -1;
     }
+
     if (radio_readFromFile(fIn, r1) == ERROR)
     {
         fprintf(stderr, "Error while reading the file.\n");
@@ -60,75 +62,77 @@ int main(int argc, char **argv)
         fclose(fIn);
         return -1;
     }
+
+    fprintf(stderr, "Radio:\n");
     radio_print(stdout, r1);
 
-    //Using DFS Algorithm
-    if (radio_depthSearch (r1, from_id, to_id) == ERROR)  {
+    /* Using DFS Algorithm */
+    if (radio_depthSearch(r1, from_id, to_id) == ERROR) {
         fprintf(stderr, "Error while using the DFS algorithm.\n");
         radio_free(r1);
         fclose(fIn);
-        stack_free(sl);
         return -1;
     }
 
-    // free r1, file and stack
+    /* Free memory */
     radio_free(r1);
     fclose(fIn);
-    stack_free(sl);
-    return 1;
+
+    return 0;
 }
 
 Status radio_depthSearch (Radio *r, long from_id, long to_id) {
-  Music *mo = NULL;
-  Music *mf = NULL;
-  Status st;
+  Music *m = NULL;
   Stack *sl = NULL;
-  int i, from, to;
+  Music *recommended = NULL;
+  int i, num;
 
-  if (!r || from_id == NO_ID || to_id == NO_ID) {
+  if (!r) return ERROR;
+
+  if (radio_contains(r, from_id) == FALSE || radio_contains(r, to_id) == FALSE) {
     return ERROR;
   }
-
-  from = radio_musicPosition(r, from_id)-1;
-  to = radio_musicPosition(r, to_id)-1;
-  mo = radio_getMusic(r, from);
-  mf = radio_getMusic(r, to);
-
+    
   fprintf (stdout, "\nFrom music with id: %ld\n", from_id);
   fprintf (stdout, "To music with id: %ld\n", to_id);
   fprintf (stdout, "Music exploration path: \n");
 
-  if (music_setState (mo, NOT_LISTENED) == ERROR || music_setState (mf, NOT_LISTENED) == ERROR) {
-    return ERROR;
-  }
-
-  st = OK;
   sl = stack_init();
 
-  if (music_setState (mo, LISTENED) == ERROR || stack_push (sl, mo) == ERROR ) {
-    return ERROR;
+  if (!sl) return ERROR;
+
+  num = radio_getNumberOfMusic(r);
+
+  /* Inicializar estados */
+  for (i = 0; i < num; i++) {
+    music_setState(radio_getMusic(r, i), NOT_LISTENED);
   }
 
-  while (stack_isEmpty(sl) == FALSE && st == OK)    
-  {
-    mo = stack_pop(sl);
-    music_plain_print(stdout, mo);
-    fprintf (stdout, "\n");
-    
-    if (music_cmp(mo, mf) == 0)   {
-        st = ERROR;
-    } 
-    
-    else {
-        for (i=from; i<to; i++)   {
-            if (radio_relationExists(r, music_getId(mo), music_getId(radio_getMusic(r, i))) == TRUE && (music_getState(radio_getMusic(r, i)) == NOT_LISTENED)) {
-                music_setState (radio_getMusic(r, i), LISTENED);
-                stack_push (sl, radio_getMusic(r, i));
-            }
+  m = radio_getMusic(r, radio_musicPosition(r, from_id));
+
+  music_setState(m, LISTENED);
+  stack_push(sl, m);
+
+  while (stack_isEmpty(sl) == FALSE) {
+    m = stack_pop(sl);
+    music_plain_print(stdout, m);
+    fprintf(stdout, "\n");
+
+    if (music_getId(m) == to_id) {
+        stack_free(sl);
+        return OK;
+    }
+
+    for (i = 0; i < num; i++) {
+        recommended = radio_getMusic(r, i);
+
+        if (radio_relationExists(r, music_getId(m), music_getId(recommended)) && music_getState(recommended) == NOT_LISTENED) {
+            music_setState(recommended, LISTENED);
+            stack_push(sl, recommended);
         }
     }
   }
-  
+
   stack_free(sl);
-  return st;
+  return OK;
 }
