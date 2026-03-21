bece77aec947580716d5050a98b3a518e8221a7f
608jorge-dev <608jorge@gmail.com>
2026-03-21 12:37:29 +0100

    casi terminado

--- material_P3/queue.c
+++ material_P3/queue.c
@@ -4,90 +4,122 @@
 
 #include "queue.h"
 
-
 struct _Queue
 {
-    void *data [MAX_QUEUE];
-    void **front;
-    void **rear;
+  void *data[MAX_QUEUE];
+  void **front;
+  void **rear;
 };
 
-Queue *queue_new()  {
-    Queue *new_queue = NULL;
-    int i;
-    new_queue = (Queue*) calloc (1, sizeof(Queue));
-    if (!new_queue) {
-        return NULL;
-    }
+Queue *queue_new()
+{
+  Queue *new_queue = NULL;
+  int i;
+  new_queue = (Queue *)calloc(1, sizeof(Queue));
+  if (!new_queue)
+  {
+    return NULL;
+  }
 
-    for (i=0; i<MAX_QUEUE; i++) {
-        new_queue->data[i] = NULL;
-    }
+  for (i = 0; i < MAX_QUEUE; i++)
+  {
+    new_queue->data[i] = NULL;
+  }
 
-    new_queue->front = &new_queue->data[0];
-    new_queue->rear = &new_queue->data[0];
+  new_queue->front = &(new_queue->data[0]);
+  new_queue->rear = &(new_queue->data[0]);
 
-    return new_queue;
+  return new_queue;
 }
 
-void queue_free(Queue *q)   {
-    q->front = NULL;
-    q->rear = NULL;
-    free (q);
-}
+void queue_free(Queue *pq) { free((void *)pq); }
 
-Bool queue_isEmpty(const Queue *q) {
-    if (!q) {
-        return FALSE;
-    } 
-
-    if (q->front == q->rear)    {
-        return TRUE;
-    }
-    
+Bool queue_isEmpty(const Queue *q)
+{
+  if (!q)
+  {
     return FALSE;
+  }
+
+  if (q->front == q->rear)
+  {
+    return TRUE;
+  }
+
+  return FALSE;
 }
 
-Status queue_push(Queue *q, void *ele) {
-    if (!q || !ele) {
-        return FALSE;
-    } 
-    
-    q->rear = ele;
-    q->rear = q->data + (q->rear+1 - q->data) % MAX_QUEUE;
-
-    return OK;
+Bool queue_isFull(const Queue *pq)
+{
+  if (pq == NULL)
+  {
+    return TRUE;
+  }
+  if ((pq->rear + 1 - pq->front) % MAX_QUEUE == 0)
+  {
+    return TRUE;
+  }
+  return FALSE;
 }
 
-void *queue_pop(Queue *q) {
-    void *ele = NULL;
-    if (!q) {
-        return FALSE;
-    } 
-    
-    ele = q->front;
-    q->front = q->data + (q->front+1 - q->data) % MAX_QUEUE;
+Status queue_push(Queue *q, void *ele)
+{
+  if (!q || !ele /*|| queue_isFull(q) == TRUE*/)
+  {
+    return FALSE;
+  }
 
-    return ele;
+  *(q->rear) = (void *)ele;
+  q->rear = q->data + (q->rear + 1 - q->data) % MAX_QUEUE;
+
+  return OK;
 }
 
-void *queue_getFront(const Queue *q) {
-    if (!q) {
-        return NULL;
-    }
+void *queue_pop(Queue *q)
+{
+  void *ele = NULL;
+  if (!q)
+  {
+    return FALSE;
+  }
 
-    return q->front;
+  ele = *(q->front);
+  *(q->front) = NULL;
+  q->front = q->data + (q->front + 1 - q->data) % MAX_QUEUE;
+
+  return ele;
 }
 
-void *queue_getBack(const Queue *q) {
-    if (!q) {
-        return NULL;
-    }
+void *queue_getFront(const Queue *q)
+{
+  if (!q)
+  {
+    return NULL;
+  }
 
-    return q->rear;
+  return *(q->front);
 }
 
-size_t queue_size(const Queue *q) {
+void *queue_getBack(const Queue *q)
+{
+  void **last_elem;
+  if ((q == NULL) || (queue_isEmpty(q) == TRUE))
+  {
+    return NULL;
+  }
+  if (q->rear == q->data)
+  {
+    last_elem = ((Queue *)q)->data + MAX_QUEUE - 1;
+  }
+  else
+  {
+    last_elem = q->rear - 1;
+  }
+  return *last_elem;
+}
+
+size_t queue_size(const Queue *q)
+{
   if (!q) {
     return 0;
   }
@@ -96,49 +128,39 @@ size_t queue_size(const Queue *q) {
     return 0;
   }
 
-  if (q->front < q->rear) {
+  if (q->front > (q->rear)) {
     return (q->front - q->rear) % MAX_QUEUE;
   }
-
-  return ((q->rear - q->front) % MAX_QUEUE);
+  
+ return (((q->rear) - q->front) % MAX_QUEUE);
 }
 
-int queue_print(FILE *fp, const Queue *q, p_queue_ele_print f) {
-  void *e=NULL, *w=NULL;
-  Queue *temp=NULL;
-  int sz;
+int queue_print(FILE *fp, const Queue *q, p_queue_ele_print f)
+{
+  Queue *temp = NULL;
+  int i, sz;
 
-  if (!fp || !q || !f)  {
+  if (!fp || !q || !f)
+  {
     return ERROR_PRINT;
   }
 
-  temp = queue_new();
-  if (!temp)  {
+  sz = queue_size(q);
+  fprintf(stdout, "%d", sz);
+  if (sz == 0)
+  {
     return ERROR_PRINT;
   }
-  sz = queue_size(q); 
-  fprintf (fp, "SIZE: %d", sz); 
 
-  /*while (queue_isEmpty(q) != TRUE){
+  for (i = 0; i < sz; i++)
+  {
     fprintf(fp, "\n");
-    e = queue_pop((void*)q);
-    if (!e) {
-      return ERROR_PRINT;
-    }
-    if (f(fp, e) == 0)  {
-      return ERROR_PRINT;
-    }
-    if (!queue_push(temp, e)) {
+    if (f(fp, q->data[i]) == 0)
+    {
       return ERROR_PRINT;
     }
   }
-
-  while (queue_isEmpty(temp)!= TRUE)  {
-    w = (void*)queue_pop(temp);
-    queue_push((void*)q,w);
-  }*/
 
   queue_free(temp);
   return 0;
 }
-    
