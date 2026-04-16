078dd999d45dd794453b801aaa0f481ae6cf0099
ireyero07 <ireyero07@gmail.com>
2026-04-16 12:22:13 +0200

    Crear material_P4

---
+++ material_P4/radio.c
@@ -0,0 +1,341 @@
+/**
+ * @file  radio.c
+ * @author Jorge Torrijos de la Cruz e Ivan Reyero Fernández
+ * @date March 2026
+ * @mersion 1.0
+ * @brief Library to manage radio
+ *
+ * @details
+ *
+ * @see
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "radio.h"
+
+struct _Radio
+{
+  Music *songs[MAX_MSC];
+  Bool relations[MAX_MSC][MAX_MSC];
+  int num_music;
+  int num_relations;
+};
+
+Radio *radio_init()
+{
+  Radio *r = NULL;
+  int i, j;
+
+  r = (Radio *)calloc(1, sizeof(Radio));
+  if (r == NULL)
+  {
+    return NULL;
+  }
+
+  r->num_music = 0;
+  r->num_relations = 0;
+
+  for (i = 0; i < MAX_MSC; i++)
+  {
+    r->songs[i] = NULL;
+    for (j = 0; j < MAX_MSC; j++)
+    {
+      r->relations[i][j] = FALSE;
+    }
+  }
+
+  return r;
+}
+
+void radio_free(Radio *r)
+{
+  int i;
+
+  if (r)
+  {
+    for (i = 0; i < r->num_music; i++)
+    {
+      music_free(r->songs[i]);
+    }
+
+    free(r);
+  }
+}
+
+Status radio_newMusic(Radio *r, char *desc)
+{
+  Music *m = NULL;
+  long id;
+
+  if (!r || !desc)
+    return ERROR;
+
+  m = music_initFromString(desc);
+  if (!m)
+    return ERROR;
+
+  id = music_getId(m);
+
+  if (radio_contains(r, id) == TRUE)
+  {
+    music_free(m);
+    return OK;
+  }
+
+  if (r->num_music >= MAX_MSC)
+  {
+    music_free(m);
+    return ERROR;
+  }
+
+  r->songs[r->num_music] = m;
+  r->num_music++;
+  music_setIndex(m, radio_getNumberOfMusic(r) - 1);
+
+  return OK;
+}
+
+Status radio_newRelation(Radio *r, long orig, long dest)
+{
+  int i, j;
+
+  if (!r || orig < 0 || dest < 0)
+    return ERROR;
+
+  i = radio_musicPosition(r, orig);
+  j = radio_musicPosition(r, dest);
+
+  if (i == NO_MUSICPOSITION || j == NO_MUSICPOSITION)
+    return ERROR;
+
+  if (radio_relationExists(r, orig, dest) == TRUE)
+  {
+    return OK;
+  }
+
+  r->relations[i][j] = TRUE;
+  r->num_relations++;
+
+  return OK;
+}
+
+Bool radio_contains(const Radio *r, long id)
+{
+  int position;
+
+  if (!r || id < 0)
+    return FALSE;
+
+  position = radio_musicPosition(r, id);
+  if (position == -1)
+  {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+int radio_getNumberOfMusic(const Radio *r)
+{
+  if (!r)
+    return NO_NUMMUSIC;
+
+  return r->num_music;
+}
+
+int radio_getNumberOfRelations(const Radio *r)
+{
+  if (!r)
+    return NO_RELATIONS;
+
+  return r->num_relations;
+}
+
+/*******************************************************/
+Bool radio_relationExists(const Radio *r, long orig, long dest)
+{
+  if (!r || orig < 0 || dest < 0)
+  {
+    return FALSE;
+  }
+  if ((radio_contains(r, orig) == FALSE) || (radio_contains(r, dest) == FALSE))
+  {
+    return FALSE;
+  }
+
+  if ((r->relations[radio_musicPosition(r, orig)][radio_musicPosition(r, dest)]) == TRUE)
+  {
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
+int radio_getNumberOfRelationsFromId(const Radio *r, long id)
+{
+  int i, rel = 0;
+  if (!r || id < 0)
+  {
+    return NO_RELATIONS;
+  }
+
+  for (i = 0; i < (radio_getNumberOfMusic(r)); i++)
+  {
+    if ((radio_relationExists(r, id, music_getId(r->songs[i]))) == TRUE)
+    {
+      rel++;
+    }
+  }
+  return rel;
+}
+
+long *radio_getRelationsFromId(const Radio *r, long id)
+{
+  long *array = NULL;
+  int i, j, size = 0;
+  if (!r || id < 0 || (radio_contains(r, id) == FALSE))
+  {
+    return NULL;
+  }
+  size = radio_getNumberOfRelationsFromId(r, id);
+  if (size == 0)
+  {
+    return NULL;
+  }
+  array = (long *)calloc(size, sizeof(long));
+  if (array == NULL)
+  {
+    return NULL;
+  }
+
+  for (i = 0, j = 0; i < (radio_getNumberOfMusic(r)); i++)
+  {
+    if (radio_relationExists(r, id, music_getId(r->songs[i])) == TRUE)
+    {
+      array[j] = music_getId(r->songs[i]);
+      j++;
+    }
+  }
+
+  return array;
+}
+
+int radio_musicPosition(const Radio *r, long id)
+{
+  int i;
+
+  if (!r || id < 0)
+  {
+    return NO_MUSICPOSITION;
+  }
+
+  for (i = 0; i < r->num_music; i++)
+  {
+    if (music_getId(r->songs[i]) == id)
+      return i;
+  }
+
+  return NO_MUSICPOSITION;
+}
+
+int radio_print(FILE *pf, const Radio *r)
+{
+  int i, j, pos;
+  long *ar = NULL;
+  if (!pf || !r)
+  {
+    return ERROR_PRINT;
+  }
+
+  for (i = 0; i < (radio_getNumberOfMusic(r)); i++)
+  {
+    /*printing music with i position in the array*/
+    music_plain_print(pf, (r->songs[i]));
+    fprintf(pf, ":");
+
+    /*printing recommendations*/
+    ar = radio_getRelationsFromId(r, music_getId(r->songs[i]));
+    if (ar == NULL)
+    {
+      return ERROR_PRINT;
+    }
+
+    for (j = 0; j < radio_getNumberOfRelationsFromId(r, music_getId(r->songs[i])); j++)
+    {
+      pos = radio_musicPosition(r, ar[j]);
+      music_plain_print(pf, r->songs[pos]);
+    }
+
+    fprintf(pf, "\n");
+    free(ar);
+
+    return OK;
+  }
+}
+
+Status radio_readFromFile(FILE *fin, Radio *r)
+{
+  char line[1024];
+  int n_music, i;
+  long orig, dest;
+  char *token = NULL;
+
+  if (!fin || !r)
+    return ERROR;
+
+  /*Read number of songs*/
+  if (!fgets(line, sizeof(line), fin))
+    return ERROR;
+
+  n_music = atoi(line);
+
+  /*Read songs*/
+  for (i = 0; i < n_music; i++)
+  {
+
+    if (!fgets(line, sizeof(line), fin))
+      return ERROR;
+
+    /*remove '\n'*/
+    line[strcspn(line, "\n")] = '\0';
+
+    if (radio_newMusic(r, line) == ERROR)
+      return ERROR;
+  }
+
+  /*Read relations*/
+  while (fgets(line, sizeof(line), fin))
+  {
+
+    token = strtok(line, " \n");
+
+    if (!token)
+      continue;
+
+    orig = atol(token);
+
+    token = strtok(NULL, " \n"); /*aqui, para el strtok, se usa NULL ya que esta funcion recuerda internamente donde se quedo*/
+
+    while (token)
+    {
+      dest = atol(token);
+
+      if (radio_newRelation(r, orig, dest) == ERROR)
+        return ERROR;
+
+      token = strtok(NULL, " \n");
+    }
+  }
+
+  return OK;
+}
+
+Music *radio_getMusic(Radio *r, int position)
+{
+  if (!r || position < 0)
+    return NULL;
+
+  return r->songs[position];
+}
